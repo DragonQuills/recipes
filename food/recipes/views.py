e2d5c35c5c1ff20fb4fee8a81c55bb20d1285376
davisc0801 <davisc1014@gmail.com>
2019-12-20 22:13:48 -0700

    WIP import from google sheets

--- food/recipes/views.py
+++ food/recipes/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
@@ -6,3 +7,13 @@ from django.urls import reverse
 
 def index(request):
     return HttpResponse("You're at the recipes index page.")
+
+def load_recipes(request):
+    from_sheets = requests.get("https://spreadsheets.google.com/feeds/cells/1MLqtrZ9gQHGK02wAtxmMogOmrhqRsUsAE5eXT1IAzOE/od6/public/values?alt=json")
+    recipes_raw = from_sheets.json()
+    recipes = []
+    for recipe in recipes_raw["feed"]["entry"]:
+        recipes.append(recipe["gs$cell"])
+
+    context = {"test": recipes}
+    return render(request, 'load_recipes.html', context)
