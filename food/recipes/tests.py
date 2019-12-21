b71155710b0fbec28a2268a955ceb6df22bd4de4
Kayden Drake <root@localhost.localdomain>
2019-12-21 13:27:31 -0700

    Modifies tests to have a make_recipes func to pop the db, and timezone stuff

--- food/recipes/tests.py
+++ food/recipes/tests.py
@@ -1,13 +1,20 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
 from .models import Recipe, Genre
 # Create your tests here.
+def make_recipes():
+    udon = Recipe(food_name = "Pork Udon", link = "https://www.bonappetit.com/recipe/stir-fried-udon-with-pork", image_link = "https://assets.bonappetit.com/photos/58c2c2bcb1bf59134d606c65/16:9/w_2560,c_limit/0317-ba-basics-stir-fried-udon-15.jpg", type = "Entree", cook_time=2, prep_time = 3, freezes_well = False, vegetarian = False, could_be_vegetarian=True, spicy = True, could_be_spicy = True, tags = "", times_made = 1, last_time_made = (timezone.now()).date() )
+
+    udon.save()
+    return udon
 
 class RecipeModelTests(TestCase):
     def test_convert_genre(self):
-        udon = Recipe(food_name = "Pork Udon", link = "https://www.bonappetit.com/recipe/stir-fried-udon-with-pork", image_link = "https://assets.bonappetit.com/photos/58c2c2bcb1bf59134d606c65/16:9/w_2560,c_limit/0317-ba-basics-stir-fried-udon-15.jpg", type = "Entree", cook_time=2, prep_time = 3, freezes_well = False, vegetarian = False, could_be_vegetarian=True, spicy = True, could_be_spicy = True, tags = "")
+        udon = make_recipes()
 
-        udon.save()
         udon.genres.create(name = "Asian")
         udon.genres.create(name = "Chinese")
 
