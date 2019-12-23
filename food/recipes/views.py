530c0fce234251cef6c4934fab62b09b2f3147aa
davisc0801 <davisc1014@gmail.com>
2019-12-23 12:10:00 -0700

    Refactor to load headers from external sheets json, rename context, strip string data

--- food/recipes/views.py
+++ food/recipes/views.py
@@ -19,30 +19,18 @@ def load_recipes(request):
     recipe_cell_data = []
     for recipe in recipes_raw["feed"]["entry"]:
         recipe_cell_data.append(recipe["gs$cell"])
-    assignment = {1:"food_name",
-                  2:"link",
-                  3:"image_link",
-                  4:"genres",
-                  5:"type",
-                  6:"cook_time",
-                  7:"prep_time",
-                  8:"freezes_well",
-                  9:"vegetarian",
-                  10:"could_be_vegetarian",
-                  11:"spicy",
-                  12:"could_be_spicy",
-                  13:"tags"}
-    index = 0
+    assignment = {}
     recipe_counter = 2
     recipe_model_data = {}
-    for x in range(recipe_cell_data.length()+1):
-        if recipe_cell_data[index]["row"] == recipe_counter:
-            recipe_model_data[assignment[recipe_cell_data[index]["col"]]] = recipe_cell_data[index]["$t"]
-        else:
+    for recipe_cell in recipe_cell_data:
+        if recipe_cell["row"] == "1":
+            assignment[recipe_cell["col"]] = recipe_cell["$t"].strip().lower().replace(" ", "_")
+        elif int(recipe_cell["row"]) == recipe_counter:
+            recipe_model_data[assignment[recipe_cell["col"]]] = recipe_cell["$t"].strip()
+        elif int(recipe_cell["row"]) == recipe_counter + 1:
             Recipe.objects.create_recipe(recipe_model_data)
             recipe_counter += 1
             recipe_model_data = {}
-            recipe_model_data[assignment[recipe_cell_data[index]["col"]]] = recipe_cell_data[index]["$t"]
-        index += 1
-    context = {"test": recipes}
-    return render(request, 'load_recipes.html', context)
+            recipe_model_data[assignment[recipe_cell["col"]]] = recipe_cell["$t"].strip()
+    context = {"new_recipes": Recipe.objects.all()}
+    return render(request, 'recipes/load_recipes.html', context)
