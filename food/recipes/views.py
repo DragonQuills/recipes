a400845dbebed45e47485f9441d4678b6ce3fc15
davisc0801 <davisc1014@gmail.com>
2020-01-11 16:35:37 -0700

    Refactor route to use a post instead of a get request to load database

--- food/recipes/views.py
+++ food/recipes/views.py
@@ -19,30 +19,34 @@ class DetailView(generic.DetailView):
     template_name = 'recipes/details.html'
 
 def load_recipes(request):
-    # call published google sheets to gather data
-    from_sheets = requests.get("https://spreadsheets.google.com/feeds/cells/1MLqtrZ9gQHGK02wAtxmMogOmrhqRsUsAE5eXT1IAzOE/od6/public/values?alt=json")
-    recipes_raw = from_sheets.json()
-    recipe_cell_data = []
-    # initial parse of data into an array, results in an array of dictonaries with row, column and cell text values
-    for recipe in recipes_raw["feed"]["entry"]:
-        recipe_cell_data.append(recipe["gs$cell"])
-    # creating 2 empty dictonaries for header assignment and model data collection. Counter starts at 2 because row 1 is the header.
-    assignment = {}
-    recipe_model_data = {}
-    recipe_counter = 2
-    for recipe_cell in recipe_cell_data:
-        if int(recipe_cell["row"]) == recipe_counter:
-            # iterate through a single row of spreadsheet data, and put that data into a dictonary with key based on headers and the value based on the spreadsheet cell.
-            recipe_model_data[assignment[recipe_cell["col"]]] = recipe_cell["$t"].strip()
-        elif int(recipe_cell["row"]) == recipe_counter + 1:
-            # after the row has finished parsing, create the model based on the data from the above row, reset the accumluator and continue to iterate.
-            Recipe.objects.create_recipe(recipe_model_data)
-            recipe_counter += 1
-            recipe_model_data = {}
-            recipe_model_data[assignment[recipe_cell["col"]]] = recipe_cell["$t"].strip()
-        elif recipe_cell["row"] == "1":
-            # iterate through first rows of cells and assign a key corrisponding to the column number and a value equal to the text.
-            assignment[recipe_cell["col"]] = recipe_cell["$t"].strip().lower().replace(" ", "_")
-    # pass all newly created objects to the template
-    context = {"new_recipes": Recipe.objects.all()}
-    return render(request, 'recipes/load_recipes.html', context)
+    if request.method == "GET":
+        context = {"recipes_not_loaded": True}
+        return render(request, 'recipes/load_recipes.html', context)
+    if request.method == "POST":
+        # call published google sheets to gather data
+        from_sheets = requests.get("https://spreadsheets.google.com/feeds/cells/1MLqtrZ9gQHGK02wAtxmMogOmrhqRsUsAE5eXT1IAzOE/od6/public/values?alt=json")
+        recipes_raw = from_sheets.json()
+        recipe_cell_data = []
+        # initial parse of data into an array, results in an array of dictonaries with row, column and cell text values
+        for recipe in recipes_raw["feed"]["entry"]:
+            recipe_cell_data.append(recipe["gs$cell"])
+        # creating 2 empty dictonaries for header assignment and model data collection. Counter starts at 2 because row 1 is the header.
+        assignment = {}
+        recipe_model_data = {}
+        recipe_counter = 2
+        for recipe_cell in recipe_cell_data:
+            if int(recipe_cell["row"]) == recipe_counter:
+                # iterate through a single row of spreadsheet data, and put that data into a dictonary with key based on headers and the value based on the spreadsheet cell.
+                recipe_model_data[assignment[recipe_cell["col"]]] = recipe_cell["$t"].strip()
+            elif int(recipe_cell["row"]) == recipe_counter + 1:
+                # after the row has finished parsing, create the model based on the data from the above row, reset the accumluator and continue to iterate.
+                Recipe.objects.create_recipe(recipe_model_data)
+                recipe_counter += 1
+                recipe_model_data = {}
+                recipe_model_data[assignment[recipe_cell["col"]]] = recipe_cell["$t"].strip()
+            elif recipe_cell["row"] == "1":
+                # iterate through first rows of cells and assign a key corrisponding to the column number and a value equal to the text.
+                assignment[recipe_cell["col"]] = recipe_cell["$t"].strip().lower().replace(" ", "_")
+        # pass all newly created objects to the template
+        context = {"new_recipes": Recipe.objects.all()}
+        return render(request, 'recipes/load_recipes.html', context)
